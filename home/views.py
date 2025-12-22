39da2485940e3f1131181d1e3f1cfde1769e2728
SilvaEsdras <esdrasesportivo@gmail.com>
2025-12-22 13:41:16 -0300

    implementação do salvar categoria

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Categoria  # Importar o modelo
+from django.shortcuts import render, redirect # Adicione o redirect
+from .models import Categoria
+from .forms import CategoriaForm # Importe o formulário
 
 def index(request):
     return render(request, 'index.html')
@@ -9,3 +10,18 @@ def categoria(request):
         'lista': Categoria.objects.all().order_by('-id'),
     }
     return render(request, 'categoria/lista.html', contexto)
+
+# Nova view para o formulário
+def form_categoria(request):
+    if request.method == 'POST':
+        form = CategoriaForm(request.POST) # pega os dados enviados pelo form
+        if form.is_valid():
+            form.save() # salva no banco
+            return redirect('categoria') # volta para a lista
+    else:
+        form = CategoriaForm() # cria form vazio
+
+    contexto = {
+        'form': form,
+    }
+    return render(request, 'categoria/formulario.html', contexto)
