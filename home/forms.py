01c2057dae2fb9c42e6dd7402de0b32e5bb58bb4
SilvaEsdras <esdrasesportivo@gmail.com>
2026-01-09 13:29:15 -0300

    Ajuste na validação dos formulários de Categoria

--- home/forms.py
+++ home/forms.py
@@ -10,14 +10,12 @@ class CategoriaForm(forms.ModelForm):
             'ordem': forms.NumberInput(attrs={'class': 'inteiro form-control', 'placeholder': ''}),
         }
 
-    # Validação personalizada para o campo 'nome' (Slide 76)
     def clean_nome(self):
         nome = self.cleaned_data.get('nome')
         if len(nome) < 3:
             raise forms.ValidationError("O nome deve ter pelo menos 3 caracteres.")
         return nome
 
-    # Validação personalizada para o campo 'ordem' (Slide 77)
     def clean_ordem(self):
         ordem = self.cleaned_data.get('ordem')
         if ordem <= 0:
